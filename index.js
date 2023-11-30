acc2184fff010c207fa0cc894f13cab6a515dc8a
Navaneeth <navaneethreddynalla@gmail.com>
2023-11-30 22:28:58 +0530

    Almost Done Frontend

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // let textValue = "print('Hello world')";
 
 let textValue = "function myScript() {\n\treturn 100;\n}\n";
+let editorDiv = document.querySelector("#editor");
 
-let myCodeMirror = CodeMirror(document.body, {
+let myCodeMirror = CodeMirror(editorDiv, {
   value: textValue,
   mode: "javascript",
   lineNumbers: true,
   indentWithTabs: true,
   indentUnit: 4,
+  showHint: true,
   theme: "monokai",
 });
