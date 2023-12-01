c7933397f04dc540f9922b0362ca173cf8d2818e
Navaneeth <navaneethreddynalla@gmail.com>
2023-12-01 17:29:53 +0530

    Changed the javascript editor to a minimal editor

--- src/editor.mjs
+++ src/editor.mjs
@@ -1,7 +1,13 @@
-import { EditorView, basicSetup } from "codemirror";
-import { javascript } from "@codemirror/lang-javascript";
+import { EditorState } from "../node_modules/@codemirror/state";
+import { EditorView, keymap } from "../node_modules/@codemirror/view";
+import { defaultKeymap } from "../node_modules/@codemirror/commands";
 
-let editor = new EditorView({
-  extensions: [basicSetup, javascript()],
+let startState = EditorState.create({
+  doc: "Hello World",
+  extensions: [keymap.of(defaultKeymap)],
+});
+
+let view = new EditorView({
+  state: startState,
   parent: document.querySelector("#editor"),
 });
