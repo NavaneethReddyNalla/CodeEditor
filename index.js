a1f0766206bc28266cc999e75d0bf5a497b4e3fb
Navaneeth <navaneethreddynalla@gmail.com>
2023-12-01 13:28:09 +0530

    Removed CodeMirror5 from the project

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-let textValue =
-  'class Hello {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}';
-let editorDiv = document.querySelector("#editor");
 
-let myCodeMirror = CodeMirror(editorDiv, {
-  value: textValue,
-  mode: "text/x-java",
-  lineNumbers: true,
-  indentWithTabs: true,
-  indentUnit: 4,
-  showHint: true,
-  theme: "monokai",
-});
