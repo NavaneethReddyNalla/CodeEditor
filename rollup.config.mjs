f3a146a0da98cc9164fe99ee202bb20b923fc30b
Navaneeth <navaneethreddynalla@gmail.com>
2023-12-01 19:36:08 +0530

    Added comments to explain the code base better:
    * Added comments to index.html to explain the script src reference
    * Added comments to editor.mjs to explain it's purpose
    * Added comments to rollup.config.mjs to explain why it has been created
    * Added comments to .gitignore to explain what is being ignored

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,24 @@
+// This is the configuration file for the rollup bundler. This config file tells the bundler to take the source code
+// from editor.mjs and bundle it with the required modules of CM6. It then generates the single js output file into
+// src/bundle/ directory as 'editor.bundle.js'.
+//
+// In package.json, rollup has been added as the dev dependencies as end users won't be using a bundler to run the program
+// instead they'll already be served with the bundled 'editor.bundle.js`
+// "scripts" has been added with the attribute {"start": "rollup -c -w"} so that when we run the command `npm start`,
+// it runs the command `rollup -c -w` which invokes rollup,
+// * the -c (short for --config) flag makes it read the configuration details from the config file(this file)
+//   and create the bundle file according to the specifications
+// * the -w (short for --watch) flag makes npm to keep a watch for changes done to the project and invoke
+//   the command every time a change is made so that we don't have to manually bundle the files all the time
+
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default {
   input: "./src/editor.mjs",
   output: {
     file: "./src/bundle/editor.bundle.js",
-    format: "iife",
+    format: "iife", // This format is immediate invokation format extension which is used to invoke the file bundled immediately upon reference
+    // It does that by adding all the bundled code into a wrapper function which is called when the file is referenced
   },
   plugins: [nodeResolve()],
 };
--- src/editor.mjs
+++ src/editor.mjs
@@ -1,3 +1,6 @@
+// This file has the extension .mjs to signify that this is a ES6 module which will later be bundled into a single file by
+// the bundler (Rollup.js in this case) and will be used by index.html to generate our code editor
+
 import { EditorView, basicSetup } from "codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 
