4afae8f3bad3b9cb97ba590b0cbdae4ab8a90871
Navaneeth <navaneethreddynalla@gmail.com>
2023-12-01 22:13:55 +0530

    Bare minimum text editor:
    * Made it Scrollable
    * Made the Default Language to be Java
    * Line numbers have to be added
    
    Co-authored-by: GKA-exe <GKA-exe@users.noreply.github.com>

--- src/editor.mjs
+++ src/editor.mjs
@@ -1,11 +1,15 @@
 // This file has the extension .mjs to signify that this is a ES6 module which will later be bundled into a single file by
 // the bundler (Rollup.js in this case) and will be used by index.html to generate our code editor
 
-import { EditorView, basicSetup } from "codemirror";
-import { javascript } from "@codemirror/lang-javascript";
+import { java } from "../node_modules/@codemirror/lang-java";
+import { EditorView, keymap } from "../node_modules/@codemirror/view";
+import {} from "../node_modules/@codemirror/commands";
+import { EditorSelection } from "../node_modules/@codemirror/state";
+
+let template = `class Hello {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}`;
 
 let view = new EditorView({
-  doc: "console.log('hello');",
-  extensions: [basicSetup, javascript()],
+  doc: template,
+  extensions: [java(), EditorView.lineWrapping],
   parent: document.querySelector("#editor"),
 });
