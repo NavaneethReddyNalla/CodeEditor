0daf1157ed46ab3b30a47ffc86edabc77cbc8bed
Navaneeth <navaneethreddynalla@gmail.com>
2023-12-02 16:27:07 +0530

    Added CSS link injections functionality
    * index.js now injects the given css files to the head tag
    * removed type="module" for editor.js script tag in index.html as it's
      not needed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 // This file injects all the necessary files into index.html for the code editor to run
+// JS file injection has been dropped because browsers no longer execute injected script for security reasons
+
+// This function injects CSS into the head tag before index.css (custom css)
+function appendCss(path, endLink) {
+  let link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = path;
+
+  document.head.insertBefore(link, endLink);
+}
+
+let pathPrefix = "../node_modules/codemirror/";
+let cssFiles = "lib/codemirror.css theme/monokai.css".split(" ");
+
+let cssEnd = document.querySelector("#global-css");
+
+// injecting css files before custom css (index.css)
+cssFiles.map((path) => appendCss(pathPrefix + path, cssEnd));
